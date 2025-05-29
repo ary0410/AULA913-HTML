7a1f3bf8ebe66dec2415e97a5874185b478075ee
Ary <mags12@aluno.ifal.edu.br>
2025-05-29 08:34:01 -0300

    Add files via upload

---
+++ JavaScript_aula/script.js
@@ -0,0 +1,17 @@
+    
+    
+alert("Hello World");
+    
+    //function soma(){
+    //console.log("Soma " + (2+3));
+    //}
+
+    function  soma(a, b){
+    console.log("Soma: " + (a + b));
+
+   
+    }
+
+    soma(4, 6);
+    soma(8.55, 9.46);
+    soma('19', '21');
